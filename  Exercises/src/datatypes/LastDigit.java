4a9206a8a96f5bbf7a41ee86c958d668e013bcf7
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-01-07 12:44:06 +0300

    Последняя цифра

---  Exercises/src/datatypes/LastDigit.java
+++  Exercises/src/datatypes/LastDigit.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
     Дано натуральное число, выведите его последнюю цифру.
  */
 
-public class LlastDigit {
+public class LastDigit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
