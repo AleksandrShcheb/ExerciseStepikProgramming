47eb6f314bbc07592a5e5eab62575bc5790a42a2
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-01-25 16:51:27 +0300

    Три города

---  Exercises/src/strexercise/ThreeCities.java
+++  Exercises/src/strexercise/ThreeCities.java
@@ -14,15 +14,35 @@ public class ThreeCities {
         String city2 = scanner.next();
         String city3 = scanner.next();
 
-        int c1  = city1.length();
-        int c2  = city2.length();
-        int c3  = city3.length();
+        int c1 = city1.length();
+        int c2 = city2.length();
+        int c3 = city3.length();
 
-        int min = Math.min(Math.min(c1,c2),c3);
-        int max = Math.max(Math.max(c1,c2),c3);
+        int min = Math.min(Math.min(c1, c2), c3);
+        int max = Math.max(Math.max(c1, c2), c3);
 
-        System.out.println(min);
-        System.out.println(max);
+        String minCity = "";
+        String maxCity = "";
+
+        if (city1.length() == min) {
+            minCity = city1;
+        }
+        if (city2.length() == min) {
+            minCity = city2;
+        }
+        if (city3.length() == min) {
+            minCity = city3;
+        }  if (city1.length() == max) {
+            maxCity = city1;
+
+        }  if (city2.length() == max) {
+            maxCity = city2;
+        }  if (city3.length() == max) {
+            maxCity = city3;
+        }
+
+        System.out.println(minCity);
+        System.out.println(maxCity);
     }
 }
 
