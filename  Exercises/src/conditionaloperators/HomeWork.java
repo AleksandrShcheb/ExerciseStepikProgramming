9e73a956146d911bdc807c1c1766dbfefbf230fb
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-01-25 20:19:04 +0300

    Домашнее задание

---
+++  Exercises/src/conditionaloperators/HomeWork.java
@@ -0,0 +1,30 @@
+package conditionaloperators;
+
+import java.util.Scanner;
+
+/*
+    Васе задали домашнее задание сделать k задач. За день Вася успевает сделать m задач.
+    Сколько дней нужно, чтобы Вася сделал домашнее задание?
+
+    Входные данные
+    Программа получает на вход числа kk и mm, каждое из которых не превышает 10001000.
+
+    Выходные данные
+    Выведите ответ на задачу.
+ */
+
+public class HomeWork {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int k = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        if (k % m == 0) {
+            System.out.println(k / m);
+        } else System.out.println(k / m + 1);
+
+
+    }
+}
+
