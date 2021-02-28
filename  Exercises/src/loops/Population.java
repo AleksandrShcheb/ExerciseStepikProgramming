d7b2c895fc541521803b09d2537d419390932204
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-02-28 10:15:40 +0300

    Популяция

---
+++  Exercises/src/loops/Population.java
@@ -0,0 +1,36 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+На вход программе подается три натуральных числа m,p,n:
+
+m: стартовое количество организмов;
+p: среднесуточное увеличение в %;
+n: количество дней для размножения.
+Напишите программу, которая предсказывает размер популяции организмов. Программа должна выводить размер
+популяции в каждый день, начиная с 1 и заканчивая n-днем.
+
+Формат входных данных
+На вход программе подается три натуральных числа.
+
+Формат выходных данных
+Программа должна вывести текст в соответствии с условием задачи.
+ */
+
+public class Population {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double m, p, n;
+        double answer =0;
+        m = scanner.nextDouble();
+        p = scanner.nextDouble();
+        n = scanner.nextDouble();
+        for (int i = 1; i < n ; i++) {
+             answer = m * Math.pow(p/100+1,i);
+            System.out.println(answer);
+        }
+    }
+}
+
