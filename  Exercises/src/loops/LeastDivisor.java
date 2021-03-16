c75f54607d007e9808018e331367ddace0d3e529
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-03-16 15:50:10 +0300

    Наименьший делитель

---  Exercises/src/loops/LeastDivisor.java
+++  Exercises/src/loops/LeastDivisor.java
@@ -1,5 +1,6 @@
 package loops;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 /*
@@ -11,16 +12,17 @@ public class LeastDivisor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int answer = 0;
+        long n = scanner.nextLong();
+
 
         for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
-                answer = i;
+                int answer = i;
+                System.out.println(answer);
                 break;
             }
         }
-        System.out.println(answer);
+
     }
 }
 
