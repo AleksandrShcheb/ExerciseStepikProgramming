a6d580349b0959b35c1639c9d6fbb42e12b8b7c6
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-03-28 19:34:19 +0300

    Табл сложения

---  Exercises/src/loops/AdditionTable.java
+++  Exercises/src/loops/AdditionTable.java
@@ -14,7 +14,9 @@ public class AdditionTable {
         for (int i = 1; i <= n ; i++) {
             for (int j = 1; j <= 9 ; j++) {
                 System.out.println(i + " + "  + j + " = " + (i+j));
+
             }
+            System.out.println();
         }
     }
 }
