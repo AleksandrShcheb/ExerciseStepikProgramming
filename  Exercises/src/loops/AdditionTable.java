518f937398044dde33ca79082c0a6fd720de9e55
AleksandrShcheb <54122711+AleksandrShcheb@users.noreply.github.com>
2021-03-28 19:30:56 +0300

    Упорядоченные цифры

---
+++  Exercises/src/loops/AdditionTable.java
@@ -0,0 +1,21 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+
+ */
+
+public class AdditionTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= 9 ; j++) {
+                System.out.println(i + " + "  + j + " = " + (i+j));
+            }
+        }
+    }
+}
+
---
+++  Exercises/src/loops/FillBar.java
@@ -0,0 +1,29 @@
+package loops;
+
+import java.util.Scanner;
+
+/*
+
+ */
+
+public class FillBar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= 9 ; j++) {
+                System.out.println(i + " + "  + j + " = " + (i+j));
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
